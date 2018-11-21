3fddf4094f6485b4af96237179bb8b8f45f916dd
Andres Redondo <afredondop@gmail.com>
2018-11-21 16:29:09 -0300

    Develop (#13)
    * Update README.md
    
    *  CircleCI + Readme badges + Gradle
    
    * Update README.md
    
    * Update README.md
    
    *  Database
    
    *  Uncapsuling booking
    
    *  Ignore warning :(
    
    *  Controlling
    
    *  Compile again
    
    *  Full Features
    
    *  Broken tests
    
    *  Improving grade
    
    *  Improving grade
    
    *  Improving grade
    
    *  Improving grade
    
    *  Improving grade with functions
    
    *  Fixing degraded
    
    *  Back 2 compile
    
    *  undegraded compile
    
    *  undegraded compile
    
    *  Enforced Optimistic Locking
    
    *  String to Long
    
    *  Codefresh
    
    *  Tests
    
    * Revert " Tests"
    
    This reverts commit 2810f8b9c124e89536e1c40e3d1d068511e4d1ba.
    
    *  Tests

---
+++ src/test/java/com/around/volcanoinn/springboot/controller/VolcanoInnTest.java
@@ -0,0 +1,122 @@
+package com.around.volcanoinn.springboot.controller;
+
+import static org.junit.Assert.assertNull;
+
+import java.net.URI;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+import com.around.volcanoinn.springboot.entity.Booking;
+import com.around.volcanoinn.springboot.entity.Campsite;
+import com.around.volcanoinn.springboot.entity.Guest;
+import com.around.volcanoinn.springboot.util.ConcurrentRequestGenerator;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class VolcanoInnTest {
+
+    // Constants
+    @LocalServerPort
+    protected static final int serverPort = 8080;
+    private static final int NUM_POOL_THREADS = 100;
+    private static final int NUM_PARALLEL_REQUESTS = 100;
+
+    private static final String REST_SERVICE_URI = "http://localhost:".concat(Integer.toString(serverPort));
+
+    @Test
+    public void inExistingCampsite() {
+
+        RestTemplate restTemplate = new RestTemplate();
+        //Not existing campsite.
+        String url = REST_SERVICE_URI + "/campsite/99";
+        Campsite campsite = restTemplate.getForObject(url, Campsite.class);
+        assertNull(campsite);
+    }
+
+    @Test
+    public void concurrentBookings() throws Exception {
+
+        Long arrivalDate = Instant.from(LocalDate.now().plusDays(7).atStartOfDay(ZoneId.systemDefault()).toInstant()).toEpochMilli();
+        Long departureDate = Instant.from(LocalDate.now().plusDays(10).atStartOfDay(ZoneId.systemDefault()).toInstant()).toEpochMilli();
+        String url = REST_SERVICE_URI + "/campsite/1/booking";
+        Booking booking = getAssignedBooking(arrivalDate, departureDate);
+        //Here we simulate concurrent booking. The load can be calc as NUM_POOL_THREADS * NUM_PARALLEL_REQUESTS
+        ConcurrentRequestGenerator generator = new ConcurrentRequestGenerator(NUM_POOL_THREADS, NUM_PARALLEL_REQUESTS, booking, url);
+        generator.run();
+    }
+
+    @Test
+    public void businessRulesMax3Days() {
+
+        Long arrivalDate = Instant.from(LocalDate.now().plusDays(4).atStartOfDay(ZoneId.systemDefault()).toInstant()).toEpochMilli();
+        Long departureDate = Instant.from(LocalDate.now().plusDays(8).atStartOfDay(ZoneId.systemDefault()).toInstant()).toEpochMilli();
+        RestTemplate restTemplate = new RestTemplate();
+        String url = REST_SERVICE_URI + "/campsite/1/booking";
+        Booking booking = getAssignedBooking(arrivalDate, departureDate);
+        URI bookingUri = getBookingUri(restTemplate, url, booking);
+        assertNull(bookingUri);
+    }
+
+    @Test
+    public void businessRulesArrivalDateToday() {
+
+        Long arrivalDate = Instant.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant()).toEpochMilli();
+        Long departureDate = Instant.from(LocalDate.now().plusDays(8).atStartOfDay(ZoneId.systemDefault()).toInstant()).toEpochMilli();
+        RestTemplate restTemplate = new RestTemplate();
+        String url = REST_SERVICE_URI + "/campsite/1/booking";
+        Booking booking = getAssignedBooking(arrivalDate, departureDate);
+        URI bookingUri = getBookingUri(restTemplate, url, booking);
+        assertNull(bookingUri);
+    }
+
+    @Test
+    public void businessRulesArrivalDateMoreThanOneMonthAhead() {
+
+        Long arrivalDate = Instant.from(LocalDate.now().plusDays(34).atStartOfDay(ZoneId.systemDefault()).toInstant()).toEpochMilli();
+        Long departureDate = Instant.from(LocalDate.now().plusDays(8).atStartOfDay(ZoneId.systemDefault()).toInstant()).toEpochMilli();
+        RestTemplate restTemplate = new RestTemplate();
+        String url = REST_SERVICE_URI + "/campsite/1/booking";
+        Booking booking = getAssignedBooking(arrivalDate, departureDate);
+        URI bookingUri = getBookingUri(restTemplate, url, booking);
+        assertNull(bookingUri);
+    }
+
+    @Test
+    public void businessRulesDepartureDateBeforeArrivalDate() {
+
+        Long arrivalDate = Instant.from(LocalDate.now().plusDays(8).atStartOfDay(ZoneId.systemDefault()).toInstant()).toEpochMilli();
+        Long departureDate = Instant.from(LocalDate.now().plusDays(4).atStartOfDay(ZoneId.systemDefault()).toInstant()).toEpochMilli();
+        RestTemplate restTemplate = new RestTemplate();
+        String url = REST_SERVICE_URI + "/campsite/1/booking";
+        Booking booking = getAssignedBooking(arrivalDate, departureDate);
+        URI bookingUri = getBookingUri(restTemplate, url, booking);
+        assertNull(bookingUri);
+    }
+
+    private URI getBookingUri(RestTemplate restTemplate, String url, Booking booking) {
+        return restTemplate.postForLocation(url, booking, Booking.class);
+    }
+
+    private Booking getAssignedBooking(Long arrivalDate, Long departureDate) {
+        Guest guest = getGuest();
+        Booking booking = getBooking(arrivalDate, departureDate);
+        booking.setGuest(guest);
+        return booking;
+    }
+
+    private Booking getBooking(Long arrivalDate, Long departureDate) {
+        return new Booking(arrivalDate, departureDate);
+    }
+
+    private Guest getGuest() {
+        return new Guest("Guest Name", "guest.email@gmail.com");
+    }
+
+}
